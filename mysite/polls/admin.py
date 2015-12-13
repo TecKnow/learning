0c5ca4341d94b571f95f2ef02422f530d78f4f08
David Perkins <tecknow@grumbleware.com>
2015-12-13 02:58:51 -0600

    Made QuestionAdmin Date Information fieldset collapse by default.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import Question
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date Information', {'fields': ['pub_date']}),
+        ('Date Information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
     ]
 
 admin.site.register(Question, QuestionAdmin)
