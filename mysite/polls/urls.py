855697480307db807fef01ccc8d6c0bba062801e
David Perkins <tecknow@grumbleware.com>
2015-12-18 18:00:03 -0600

    Converted Polls to generic views.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,14 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /polls/
-    url(r'^$', views.index, name='index'),
-    # /polls/id/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # polls/id/results
-    url(r'^(?P<question_id>[0-9]+)/results/$',
-        views.results,
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$',
+        views.DetailView.as_view(),
+        name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$',
+        views.ResultsView.as_view(),
         name='results'),
-    # polls/id/vote
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
