91130a6912694c4395264dbaa177825b133141e0
David P. Perkins <tecknow@grumbleware.com>
2018-10-06 20:59:37 -0500

    Code after video 10

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 // import ReactDOM from "react-dom";
 import "./index.css";
 // import { createStore } from "redux";
-import expect from 'expect';
-import deepFreeze from 'deep-freeze';
+import expect from "expect";
+import deepFreeze from "deep-freeze";
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
@@ -14,46 +14,25 @@ import deepFreeze from 'deep-freeze';
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-const addCounter = (list) => {
-	return [...list, 0];
+const toggleTodo = todo => {
+  return { ...todo, completed: !todo.completed };
 };
 
-const removeCounter = (list, index) =>{
-	return [...list.slice(0, index), ...list.slice(index+1)];
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: "Learn Redux",
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: "Learn Redux",
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+  expect(toggleTodo(todoBefore)).toEqual(todoAfter);
 };
 
-const incrementCounter = (list, index) => {
-	return [...list.slice(0, index), list[index]+1, ...list.slice(index+1)];
-};
-
-const testAddCounter = () => {
-	const listBefore = [];
-	const listAfter = [0];
-
-	deepFreeze(listBefore);
-	expect(
-		addCounter(listBefore)).toEqual(listAfter);
-};
-
-const testRemoveCounter = () => {
-	const listBefore = [0, 10, 20];
-	const listAfter = [0, 20];
-
-	deepFreeze(listBefore);
-
-	expect(removeCounter(listBefore, 1)).toEqual(listAfter);
-};
-
-const testIncrementCounter = () => {
-	const listBefore = [0, 10,20];
-	const listAfter = [0, 11, 20];
-
-	deepFreeze(listBefore);
-
-	expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
-};
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-console.log("All tests passed");
+testToggleTodo();
+console.log("All tests passed.");
