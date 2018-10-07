e52aefe6151eef1aa877780e2bb8ba2475e6a3a7
David P. Perkins <tecknow@grumbleware.com>
2018-10-06 20:46:59 -0500

    Code after video 9

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 import "./index.css";
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
@@ -12,39 +14,46 @@ import { createStore } from "redux";
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-      break;
-    case "DECREMENT":
-      return state - 1;
-      break;
-    default:
-      return state;
-  }
+const addCounter = (list) => {
+	return [...list, 0];
 };
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
-
-const store = createStore(counter);
-
-export const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-    />,
-    document.getElementById("root")
-  );
+const removeCounter = (list, index) =>{
+	return [...list.slice(0, index), ...list.slice(index+1)];
 };
 
-store.subscribe(render);
-render();
+const incrementCounter = (list, index) => {
+	return [...list.slice(0, index), list[index]+1, ...list.slice(index+1)];
+};
+
+const testAddCounter = () => {
+	const listBefore = [];
+	const listAfter = [0];
+
+	deepFreeze(listBefore);
+	expect(
+		addCounter(listBefore)).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+	const listBefore = [0, 10, 20];
+	const listAfter = [0, 20];
+
+	deepFreeze(listBefore);
+
+	expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+};
+
+const testIncrementCounter = () => {
+	const listBefore = [0, 10,20];
+	const listAfter = [0, 11, 20];
+
+	deepFreeze(listBefore);
+
+	expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log("All tests passed");
