00948a4c0ab9b0070a8ccfed14c373593ae230fb
David P. Perkins <tecknow@grumbleware.com>
2018-10-26 17:10:22 -0500

    Code after video 13.

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+import uuidv4 from "uuid/v4";
+
+const fakeDatabase = {
+  todos: [
+    {
+      id: uuidv4(),
+      text: "hey",
+      completed: true
+    },
+    {
+      id: uuidv4(),
+      text: "ho",
+      completed: true
+    },
+    {
+      id: uuidv4(),
+      text: "let’s go",
+      completed: false
+    }
+  ]
+};
+
+const delay = ms => new Promise((resolve, reject) => setTimeout(resolve, ms));
+
+export const fetchTodos = filter =>
+  delay(500).then(() => {
+    switch (filter) {
+      case "all":
+        return fakeDatabase.todos;
+      case "active":
+        return fakeDatabase.todos.filter(t => !t.completed);
+      case "completed":
+        return fakeDatabase.todos.filter(t => t.completed);
+      default:
+        throw new Error(`Unknown filter: ${filter}`);
+    }
+  });
