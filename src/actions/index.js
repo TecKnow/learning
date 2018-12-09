7053add6de0705d41181c7dcb9751d8c4dfec97e
David P. Perkins <tecknow@grumbleware.com>
2018-12-08 22:29:50 -0600

    Code after video 25.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
-import uuidv4 from "uuid/v4";
 import { getIsFetching } from "../reducers";
 import * as api from "../api";
 
-export const addTodo = text => ({
-  type: "ADD_TODO",
-  text: text,
-  id: uuidv4()
-});
+export const addTodo = text => dispatch => {
+  api
+    .addTodo(text)
+    .then(response => dispatch({ type: "ADD_TODO_SUCCESS", response }));
+};
 export const toggleTodo = id => ({
   type: "TOGGLE_TODO",
   id
@@ -21,18 +20,21 @@ export const fetchTodos = filter => (dispatch, getState) => {
     filter
   });
 
-  return api.fetchTodos(filter).then(response => {
-    dispatch({
-      type: "FETCH_TODOS_SUCCESS",
-      filter,
-      response
-    });
-  }, error =>{
-    dispatch({type: "FETCH_TODOS_FAILURE",
-    error: true,
-    filter,
-    message: error.message  || "Something went wrong."
-  });
-
-  });
+  return api.fetchTodos(filter).then(
+    response => {
+      dispatch({
+        type: "FETCH_TODOS_SUCCESS",
+        filter,
+        response
+      });
+    },
+    error => {
+      dispatch({
+        type: "FETCH_TODOS_FAILURE",
+        error: true,
+        filter,
+        message: error.message || "Something went wrong."
+      });
+    }
+  );
 };
