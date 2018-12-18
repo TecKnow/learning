4e2c827d1fd6d503bac94f3661f4a00d5be54c2c
David P. Perkins <tecknow@grumbleware.com>
2018-12-18 17:29:36 -0600

    Code after video 5.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import RegisterForm from './components/RegisterForm';
 
 class RegisterFormContainer extends Component {
+
   submit = values => {
     window.alert(JSON.stringify(values, null, 4));
   }
+
+  getInitialValues = () =>({
+    preference: "spaces",
+    newsletter: true
+    })
   render(){
-    return <RegisterForm onSubmit={this.submit} />
+    return <RegisterForm onSubmit={this.submit} initialValues={this.getInitialValues()} />
   }
 }
 
