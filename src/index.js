f366166a9cc86943f4e3d4452de841e031389394
David P. Perkins <tecknow@grumbleware.com>
2019-01-04 23:18:47 -0600

    Code after video 7.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@atlaskit/css-reset';
-import { DragDropContext } from 'react-beautiful-dnd';
-import initialData from './initial-data';
-import Column from './column';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import { DragDropContext } from "react-beautiful-dnd";
+import initialData from "./initial-data";
+import Column from "./column";
 
 class App extends React.Component {
   state = initialData;
 
   onDragEnd = result => {
-  	const {destination, source, draggableId } = result;
-  	if(!destination){
-  		return;
-  	}
-  	if(destination.droppableId === source.droppableId && destination.index === source.index){
-  		return;
-  	}
-  	const column = this.state.columns[source.droppableId];
-  	const newTaskIds = Array.from(column.taskIds);
-  	newTaskIds.splice(source.index, 1);
-  	newTaskIds.splice(destination.index, 0, draggableId);
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
 
-  	const newColumn = {...column, taskIds: newTaskIds};
-  	const newState = {
-  		...this.state,
-  		columns: {
-  			...this.state.columns,
-  			[newColumn.id]: newColumn
-  		},
-  	};
-  	this.setState(newState);
+    const newColumn = { ...column, taskIds: newTaskIds };
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+    this.setState(newState);
+  };
+
+  onDragStart = () => {
+    document.body.style.color = "orange";
+  };
+
+  onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+    document.body.style.transition = "background-color 0.2s ease";
   };
 
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragEnd={this.onDragEnd}
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+      >
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
@@ -46,4 +68,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
