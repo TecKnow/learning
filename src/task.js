0b58297f31d85d67786a37ff77dd6a7320fd1d63
David P. Perkins <tecknow@grumbleware.com>
2019-01-09 03:22:39 -0600

    Code after video 11.

--- src/task.js
+++ src/task.js
@@ -3,16 +3,27 @@ import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
-  border: 1px solid lightgrey;
-  border-radius: 2px;
+  border: 3px solid lightgrey;
+  border-radius: 50%;
   padding: 8px;
-  margin-bottom: 8px;
+  margin-right: 8px;
   background-color: ${props =>
     props.isDragDisabled
       ? "lightgrey"
       : props.isDragging
         ? "lightgreen"
         : "white"};
+  width: 40px;
+  height 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus {
+  	outline: none;
+  	border-color: red;
+  }
+
 `;
 
 export default class Task extends React.Component {
@@ -32,7 +43,7 @@ export default class Task extends React.Component {
             isDragging={snapshot.isDragging}
             isDragDisabled={isDragDisabled}
           >
-            {this.props.task.content}
+            {this.props.task.content[0]}
           </Container>
         )}
       </Draggable>
