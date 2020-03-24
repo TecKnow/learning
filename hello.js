946e3b8e27982ba51299197df0eac7fa7b3fcfb4
David P. Perkins <tecknow@grumbleware.com>
2020-03-24 17:30:25 -0500

    Code after lesson 5.
    *  Introduces NPM to the project.
        *  Adds package.json and package-lock.json to git.
        *  Adds node_modules/ to .gitignore.
        *  Adds package-lock.json to ignored files for the Sublime project

--- hello.js
+++ hello.js
@@ -1,8 +1,11 @@
 var http = require("http");
+var moment = require("moment");
 
 function serverCallback(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello " + process.argv[2] + "!");
+  res.end(
+    "Hello " + process.argv[2] + " it is " + moment().format("LLLL") + " now"
+  );
 }
 
 http.createServer(serverCallback).listen(8080);
