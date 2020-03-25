cd722a37e22acc14f543a741918cd0a6cb62733a
David P. Perkins <tecknow@grumbleware.com>
2020-03-25 17:37:41 -0500

    Code after lesson 10.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let http = require("http");
 let request = require("request");
 let fs = require("fs");
+let csv = require("csv");
 
 let request_body = undefined;
 let html_content = undefined;
@@ -36,19 +37,57 @@ function createHtmlStringFromJson(retrievedData) {
   return string_until_body + html_string + string_from_body;
 }
 
-request("https://www.bnefoodtrucks.com.au/api/1/trucks", function (
-  err,
-  request_res,
-  body
-) {
-  request_body = body;
-});
+function createHtmlStringFromCsv(retrievedData) {
+  let body_begin_index = html_content.indexOf("<body>");
+  let body_end_index = html_content.indexOf("</body>");
+
+  let string_until_body =
+    html_content.slice(0, body_begin_index) + "<body>".length;
+  let string_from_body = html_content.slice(body_end_index);
+
+  let html_string = "<table><tr>";
+  retrievedData[0].forEach(function (attribute) {
+    html_string += "<th>" + attribute + "</th>";
+  });
+  html_string += "</tr>";
+  let data = retrievedData.slice(1);
+
+  data.forEach(function (row) {
+    html_string += "<tr>";
+    row.forEach(function (cell) {
+      html_string += "<td>" + cell + "</td>";
+    });
+    html_string += "</tr>";
+  });
+
+  html_string += "</table>";
+
+  return string_until_body + html_string + string_from_body;
+}
+
+// request("https://www.bnefoodtrucks.com.au/api/1/trucks", function (
+//   err,
+//   request_res,
+//   body
+// ) {
+//   request_body = body;
+// });
+
+request(
+  "https://www.data.brisbane.qld.gov.au/data/dataset/1e11bcdd-fab1-4ec5-b671-396fd1e6dd70/resource/3c972b8e-9340-4b6d-8c7b-2ed988aa3343/download/public-art-open-data-2020-01-09.csv",
+  function (err, request_res, body) {
+    csv.parse(body, function (err, data) {
+      request_body = data;
+    });
+  }
+);
 
 http
   .createServer(function (req, res) {
     if (request_body && html_content) {
       res.writeHead(200, { "content-type": "text/html" });
-      res.end(createHtmlStringFromJson(JSON.parse(request_body)));
+      // res.end(createHtmlStringFromJson(JSON.parse(request_body)));
+      res.end(createHtmlStringFromCsv(request_body));
     } else {
       res.writeHead(200, { "content-type": "text/plain" });
       res.end("Nothing retrieved yet");
