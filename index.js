09f29d2ba0937446e194c439bc9d618c4d96c533
David P. Perkins <tecknow@grumbleware.com>
2020-03-24 21:05:13 -0500

    Code after lesson 8.

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ let request = require("request");
 
 let request_body = undefined;
 
+function createHtmlStringFromJson(retrievedData) {
+  let html_string =
+    "<html><header><title>Data aggregator</title></header></html><body><table>";
+  html_string += "<tr>";
+  for (let attribute in retrievedData[0]) {
+    if (typeof retrievedData[0][attribute] !== "object") {
+      html_string += "<th>" + attribute + "</th>";
+    }
+  }
+  html_string += "</tr>";
+
+  retrievedData.forEach(function (obj) {
+    html_string += "<tr>";
+    for (let attr in obj) {
+      if (typeof obj[attr] !== "object") {
+        html_string += "<td>" + obj[attr] + "</td>";
+      }
+    }
+    html_string += "</tr>";
+  });
+
+  html_string += "</table></body></html>";
+
+  return html_string;
+}
+
 request("https://www.bnefoodtrucks.com.au/api/1/trucks", function (
   err,
   request_res,
@@ -11,12 +37,14 @@ request("https://www.bnefoodtrucks.com.au/api/1/trucks", function (
   request_body = body;
 });
 
-http.createServer(function (req, res) {
-  if (request_body) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end(request_body);
-  } else {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Nothing retrieved yet");
-  }
-}).listen(8080);
+http
+  .createServer(function (req, res) {
+    if (request_body) {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(createHtmlStringFromJson(JSON.parse(request_body)));
+    } else {
+      res.writeHead(200, { "content-type": "text/plain" });
+      res.end("Nothing retrieved yet");
+    }
+  })
+  .listen(8080);
