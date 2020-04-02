ea634ec3c617a673c87feba9be29a789a1884eec
David P. Perkins <tecknow@grumbleware.com>
2020-04-02 16:24:48 -0500

    Code after lesson 3.
    It turns out that the version of Babel that this project is using (6.*) doesn't process config files if they have an extension.  So .babelrc.json needed to become .babelrc.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+import express from "express";
+import parser from "body-parser";
+
+const app = new express();
+app.use(parser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/world", (req, res) => {
+  res.send("World!");
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
+app.listen(process.env.PORT || 3000);
+console.log("Listening");
