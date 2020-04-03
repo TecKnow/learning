03149ea31c92daaae54278c2fd2d03414db182e0
David P. Perkins <tecknow@grumbleware.com>
2020-04-03 18:28:03 -0500

    Code after lesosn 4.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import express from "express";
 import parser from "body-parser";
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://tutorial_user:L5PFftSGykgmbAgs@node-express-jwt-test-awwma.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const userSchema = new Schema({
+  username: { type: String, required: true, index: { unique: true } },
+});
+
+const User = mongoose.model("user", userSchema);
 
 const app = new express();
 app.use(parser.urlencoded({ extended: false }));
@@ -8,12 +23,20 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/world", (req, res) => {
-  res.send("World!");
+app.get("/users", (req, res) => {
+  User.find({}, function (err, users) {
+    res.send(users);
+  });
 });
 
 app.post("/", (req, res) => {
-  res.send(req.body);
+  const { username } = req.body;
+  const newUser = new User({
+    username,
+  });
+  newUser.save(function (err, model) {
+    res.send(model);
+  });
 });
 app.listen(process.env.PORT || 3000);
 console.log("Listening");
