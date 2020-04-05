4e0a90041dba4f8a18d7864a6cf51a41292ca81c
David P. Perkins <tecknow@grumbleware.com>
2020-04-05 17:40:03 -0500

    Code after lesson 7.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,38 @@
 import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 import mongoose from "../database/mongodb";
 
 const userSchema = new Schema({
   username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
 });
+
+function updatePassword(user, next) {
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+}
+
+userSchema.pre("save", function (next) {
+  updatePassword(this, next);
+});
+
+userSchema.methods.comparePassword = function (password, next) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (err) {
+      return next(err);
+    }
+    return next(null, isMatch);
+  });
+};
 
 export default mongoose.model("user", userSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,10 +8,44 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/password", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send({
+      err: `Required fields not found: ${!username ? "username" : ""} ${
+        !password ? "password" : ""
+      }`,
+    });
+  }
+  User.findOne({ username }, function (err, userModel) {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    if (!userModel) {
+      return res.status(400).send({ err: "Cannot find user" });
+    }
+    return userModel.comparePassword(password, function (err, isMatch) {
+      if (err) {
+        return res.status(400).send(err);
+      }
+      return res.send({ correct: isMatch });
+    });
+  });
+});
+
 router.post("/", (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send({
+      err: `Required fields not found: ${!username ? "username" : ""} ${
+        !password ? "password" : ""
+      }`,
+    });
+  }
+
   const newUser = new User({
     username,
+    password,
   });
   newUser.save(function (err, model) {
     res.send(model);
