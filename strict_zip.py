0766b9889f2b0b3f8278040176d096fc47316170
David P. Perkins <tecknow@grumbleware.com>
2020-05-25 16:49:56 -0500

    Passing first bonus objective

--- strict_zip.py
+++ strict_zip.py
@@ -1,6 +1,7 @@
-def strict_zip(*sequences):
-    lengths = {len(s) for s in sequences}
+def strict_zip(*iterables):
+    iterables = [list(s) for s in iterables]
+    lengths = {len(s) for s in iterables}
     if len(lengths) > 1:
         raise ValueError()
     else:
-        return list(zip(*sequences))
+        return list(zip(*iterables))
