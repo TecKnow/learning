0465bdfea85d8f6d7ab33355c515754ea9412360
David P. Perkins <tecknow@grumbleware.com>
2020-06-09 17:19:31 -0500

    Passing all bonus objectives.

--- chunked.py
+++ chunked.py
@@ -1,32 +1,13 @@
-class chunked:
+from itertools import chain, islice, repeat
+
+_DEFAULT_FILLVALUE = object()
+
+
+def chunked(iterable, n, *, fill=_DEFAULT_FILLVALUE):
     _EOL = object()
-    _INITIAL_VALUE = object()
-    _DEFAULT_FILLVALUE = object()
-
-    def __init__(self, iterable, n, *, fill=_EOL):
-        self.it = iter(iterable)
-        self.n = n
-        self.fill = fill
-        self.curvalue = self._INITIAL_VALUE
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.curvalue is self._INITIAL_VALUE or self.curvalue is self._EOL:
-            self.curvalue = next(self.it)
-        return self._chunker()
-
-    def _chunker(self):
-        for _ in range(self.n):
-            yield self.curvalue
-            try:
-                self.curvalue = next(self.it)
-            except StopIteration:
-                self.curvalue = self._EOL
-                return
-
-if __name__ == "__main__":
-    squares = (n ** 2 for n in range(10))
-    output = chunked(squares, 4)
-    print(*output)
+    iterator = iter(iterable)
+    padded_iterator = iterator if fill is _DEFAULT_FILLVALUE else chain(iterator, repeat(fill))
+    while (currentValue := next(iterator, _EOL)) is not _EOL:
+        restored_iterator = chain((currentValue,), padded_iterator)
+        yield islice(restored_iterator, n)
+    return
