7ed4142cb4edd1b3e44f64c840c4d8c4c20df233
David P. Perkins <tecknow@grumbleware.com>
2020-06-10 14:40:56 -0500

    Experimenting with Python typing.

--- chunked.py
+++ chunked.py
@@ -1,12 +1,13 @@
 from itertools import chain, islice, repeat
+from typing import Any, Iterable, Iterator
 
-_DEFAULT_FILLVALUE = object()
+_DEFAULT_FILL_VALUE = object()
 
 
-def chunked(iterable, n, *, fill=_DEFAULT_FILLVALUE):
+def chunked(iterable: Iterable[Any], n: int, *, fill: Any = _DEFAULT_FILL_VALUE) -> Iterator[Iterator[Any]]:
     _EOL = object()
     iterator = iter(iterable)
-    padded_iterator = iterator if fill is _DEFAULT_FILLVALUE else chain(iterator, repeat(fill))
+    padded_iterator = iterator if fill is _DEFAULT_FILL_VALUE else chain(iterator, repeat(fill))
     while (currentValue := next(iterator, _EOL)) is not _EOL:
         restored_iterator = chain((currentValue,), padded_iterator)
         yield islice(restored_iterator, n)
