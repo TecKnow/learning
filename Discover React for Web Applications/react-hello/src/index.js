0a21a8b833c01e38c60befe22024b35aabd342c5
David P. Perkins <tecknow@grumbleware.com>
2020-09-26 15:33:48 -0500

    Discover React for Web Applications code after lesson 4.

--- Discover React for Web Applications/react-hello/src/index.js
+++ Discover React for Web Applications/react-hello/src/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function HelloWorld() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
 }
+
+function Hello() {
+  const name = "World!"
+  return <span>Hello {name}</span>;
+}
+
+// function World() {
+//   return <span>world</span>;
+// }
 
 ReactDom.render(<HelloWorld />, document.querySelector("#root"));
