8c7e1d01f6de8126682343613ce674fd4305bccb
David P. Perkins <tecknow@grumbleware.com>
2020-09-26 20:26:23 -0500

    Discover React for Web Applications code after lesson 12.

--- Discover React for Web Applications/shopper/src/App.js
+++ Discover React for Web Applications/shopper/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import "./App.css";
 
 const App = () => {
-  return(
+  const [activeTab, setActiveTab] = useState("items");
+  return (
     <div className="App">
-      <Nav/>
+      <Nav activeTab={activeTab} onTabChange={setActiveTab}/>
       <main className="app-content">
-      <span>Empty</span>
+        <Content tab={activeTab} />
       </main>
     </div>
-    );
-}
+  );
+};
+
+const Content = ({ tab }) => {
+  switch (tab) {
+    default:
+    case "items":
+      return <span>The items</span>;
+    case "cart":
+      return <span>The cart</span>;
+  }
+};
 
 export default App;
