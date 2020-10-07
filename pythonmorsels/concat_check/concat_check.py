f5eef8da974c8256fc0c2eb5aa97a35b437c5868
David P. Perkins <tecknow@grumbleware.com>
2020-10-07 15:52:51 -0500

    Update parser to use token.NL instead of token.NEWLINE
    token.NL is the token for a newline that doesn't end a statement.  Unofrtunately, this still doesn't work.

--- pythonmorsels/concat_check/concat_check.py
+++ pythonmorsels/concat_check/concat_check.py
@@ -3,7 +3,7 @@
 import tokenize
 from io import StringIO
 from textwrap import dedent
-from token import STRING, NEWLINE
+from token import STRING, NL
 import sys
 
 
@@ -15,7 +15,8 @@ def concat_check_files_messages(file_names):
 def concat_check_files(file_names):
     yield from ((file_name, line) for file_name in file_names for line in concat_check_file(tokenize.open(file_name)))
 
-
+# I learned that token.NL is the token for a newline that does not end a statement from the reference solutions for this
+# exercise, but I am updating it because I want a record of this information if I ever come back to this.
 def concat_check_file(file_handle):
     tokens = tokenize.generate_tokens(file_handle.readline)
     open_string = None
@@ -24,7 +25,7 @@ def concat_check_file(file_handle):
             open_string = token
         elif open_string is not None and token[0] == STRING:
             yield open_string[3][0]
-        elif open_string is not None and token[0] != NEWLINE:
+        elif open_string is not None and token[0] != NL:
             open_string = None
 
 
