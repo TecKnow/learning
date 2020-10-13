2e8e8c64698ece69a900da59998e4405f56d6c55
David P. Perkins <tecknow@grumbleware.com>
2020-10-13 17:56:24 -0500

    Pythonmorsels csv_headers passing base, 1st, 2nd bonus objectives

---
+++ pythonmorsels/csv_columns/csv_columns.py
@@ -0,0 +1,11 @@
+from csv import DictReader
+from typing import TextIO, Dict, Any
+
+
+def csv_columns(input_file: TextIO, *, headers=None, missing=None) -> Dict[Any, Any]:
+    csv_dict_reader = DictReader(input_file, fieldnames=headers)
+    result_dict = dict()
+    for row in csv_dict_reader:
+        for key, value in row.items():
+            result_dict.setdefault(key, list()).append(value)
+    return result_dict
