605649b5f5bdcb9ff4928a67904160b5a6b5aaa5
David P. Perkins <tecknow@grumbleware.com>
2020-10-13 19:49:17 -0500

    Pythonmorsels csv_headers passing all objectives

--- pythonmorsels/csv_columns/csv_columns.py
+++ pythonmorsels/csv_columns/csv_columns.py
@@ -3,7 +3,7 @@ from typing import TextIO, Dict, Any
 
 
 def csv_columns(input_file: TextIO, *, headers=None, missing=None) -> Dict[Any, Any]:
-    csv_dict_reader = DictReader(input_file, fieldnames=headers)
+    csv_dict_reader = DictReader(input_file, fieldnames=headers, restval=missing)
     result_dict = dict()
     for row in csv_dict_reader:
         for key, value in row.items():
