22b8239f983f53fbf1aef5b0f5368ff9d9f6e6b5
David P. Perkins <tecknow@grumbleware.com>
2020-10-15 15:08:05 -0500

    Pythonmorsels make_file passing all objectives.

--- pythonmorsels/make_file/make_file.py
+++ pythonmorsels/make_file/make_file.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 import tempfile
 from contextlib import contextmanager
+import locale
 
 @contextmanager
-def make_file(contents=None):
+def make_file(contents=None, *, directory=None, **options):
     try:
-        named_temp_file = tempfile.NamedTemporaryFile(delete=False)
+        if "mode" not in options:
+            options["mode"] = "a"
+        named_temp_file = tempfile.NamedTemporaryFile(delete=False, dir=directory, **options)
         if contents is not None:
             named_temp_file.write(contents)
         named_temp_file.close()
         yield named_temp_file.name
     finally:
+        named_temp_file.close()
         Path(named_temp_file.name).unlink()
 
