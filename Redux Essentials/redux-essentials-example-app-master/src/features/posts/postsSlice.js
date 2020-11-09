9c2f87369d52ca53fd567213882e614057088c88
David P. Perkins <tecknow@grumbleware.com>
2020-11-08 20:31:14 -0600

    Redux Essentials code after part 4

--- Redux Essentials/redux-essentials-example-app-master/src/features/posts/postsSlice.js
+++ Redux Essentials/redux-essentials-example-app-master/src/features/posts/postsSlice.js
@@ -1,9 +1,21 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
-import {sub} from 'date-fns'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), {minutes: 10}).toISOString() },
-  { id: '2', title: 'Second Post', content: 'More text', date: sub(new Date(), {minutes: 5}).toISOString() },
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello!',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {}
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {}
+  },
 ]
 
 const postsSlice = createSlice({
@@ -22,11 +34,12 @@ const postsSlice = createSlice({
             title,
             content,
             user: userId,
+            reactions:{}
           },
         }
       },
     },
-    postUpdated(state, action) {
+    postUpdated: (state, action) => {
       const { id, title, content } = action.payload
       const existingPost = state.find((post) => post.id === id)
       if (existingPost) {
@@ -34,9 +47,17 @@ const postsSlice = createSlice({
         existingPost.content = content
       }
     },
+    reactionAdded: (state, action) => {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        const starting_reactions = existingPost.reactions[reaction] || 0
+        existingPost.reactions[reaction] = starting_reactions + 1
+      }
+    },
   },
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
