c7b1b4b8df27d3fb28ce15e58771fd036ace9480
David P. Perkins <tecknow@grumbleware.com>
2020-12-05 01:20:41 -0600

    Advent of code day 5 part 2.

--- Advent of Code 2020/Day 5 - Binary Boarding.py
+++ Advent of Code 2020/Day 5 - Binary Boarding.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 def decode_boarding_pass(boarding_pass: str) -> dict[str, int]:
     boarding_pass = boarding_pass.replace("F", "0")
     boarding_pass = boarding_pass.replace("B", "1")
@@ -18,8 +15,25 @@ def decode_boarding_pass(boarding_pass: str) -> dict[str, int]:
     }
 
 
+def find_missing_seats(decoded_boarding_passes: dict[str, int]) -> set[int]:
+    all_seats = set(range(1024))
+    known_seats = set([boarding_pass["ID"] for boarding_pass in decoded_boarding_passes])
+    missing_seats = all_seats.difference(known_seats)
+    return missing_seats, known_seats
+
+
+def find_potential_seats(missing_seats: set[int], known_seats: set[int]) -> set[int]:
+    result_set = set()
+    for missing_seat in missing_seats:
+        if missing_seat + 1 in known_seats and missing_seat - 1 in known_seats:
+            result_set.add(missing_seat)
+    return result_set
+
 if __name__ == "__main__":
     data = open("Day 5 Data.txt").readlines()
     decoded_data = [decode_boarding_pass(row) for row in data]
     max_id = max(x["ID"] for x in decoded_data)
     print(f"Maximum ID found: {max_id}")
+    missing_seats, known_seats = find_missing_seats(decoded_data)
+    potential_seats = find_potential_seats(missing_seats, known_seats)
+    print(f"{potential_seats=}")
