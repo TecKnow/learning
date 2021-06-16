e62f7e06f2186eadda98b84926dc5dc3ce9e1021
David P. Perkins <tecknow@grumbleware.com>
2021-06-16 18:05:33 -0500

    Solution for the PythonMorsels problem PIecewiseRange passing base tests

---
+++ pythonmorsels/PiecewiseRange/piecewiserange.py
@@ -0,0 +1,20 @@
+class PiecewiseRange:
+
+    def __init__(self, ranges: str):
+        elements = ranges.split(",")
+        elements = [elem.split("-") for elem in elements]
+        elements = [[int(x) for x in element] for element in elements]
+        self.elements = elements
+
+    def __iter__(self):
+        for element in self.elements:
+            if len(element) == 1:
+                yield element[0]
+            else:
+                yield from range(element[0], element[1]+1)
+
+
+if __name__ == "__main__":
+    p = PiecewiseRange('1-2, 4, 8-10, 11')
+    pl = list(p)
+    print(pl)
