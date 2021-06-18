c78bbddba76f7fe6da1b792d5e36d3806422d524
David P. Perkins <tecknow@grumbleware.com>
2021-06-18 17:39:38 -0500

    Solution for the PythonMorsels problem update_reviews passing second bonus objective

--- pythonmorsels/update_reviews/update_reviews.py
+++ pythonmorsels/update_reviews/update_reviews.py
@@ -23,12 +23,19 @@ if __name__ == "__main__":
         update_list = [*update_reader]
         records = {(r["State"], r["City"], r["Name"], r["Street"]): r for r in master_list}
         rows_added = 0
+        rows_updated = 0
         for row in update_list:
             if (row["State"], row["City"], row["Name"], row["Street"]) not in records:
                 rows_added += 1
                 records[(row["State"], row["City"], row["Name"], row["Street"])] = row
+            elif args["update"]:
+                if row["Comments"]:
+                    records[(row["State"], row["City"], row["Name"], row["Street"])]["Comments"] = row["Comments"]
+                    rows_updated += 1
     with open(args["master.csv"], 'w', newline='') as outfile:
         writer = csv.DictWriter(outfile, fieldnames=COLUMN_NAMES.split())
         writer.writeheader()
         writer.writerows(records.values())
     print(f"Added {rows_added} row(s)")
+    if args["update"]:
+        print(f"Updated {rows_updated} row(s)")
