b36ed2a2e2dfded5347966b61817a77d3ecfa313
David P. Perkins <tecknow@grumbleware.com>
2021-07-12 21:48:15 -0500

    Solution for Pythonmorsels problem "final_class" passing all objectives.

--- pythonmorsels/final_class/final_class.py
+++ pythonmorsels/final_class/final_class.py
@@ -1,5 +1,24 @@
+import functools
 
+
+class UnsubclassableType(type):
+    def __new__(mcs, name, bases, dct, *args, **kwargs):
+        x = super().__new__(mcs, name, bases, dct, *args, **kwargs)
+        for parent_class in bases:
+            if isinstance(parent_class, mcs):
+                raise TypeError(f"{parent_class.__name__} is not an appropriate base class")
+        return x
+
+
+def final_class(cls):
+    """Make a class unavailable for subclassing"""
+
+    class NewClass(cls, metaclass=UnsubclassableType):
+        pass
+
+    return NewClass
+
+
+@final_class
 class Unsubclassable():
-    @classmethod
-    def __init_subclass__(cls, **kwargs):
-        raise TypeError(f"{cls.__name__} is not an appropriate parent class")
+    pass
