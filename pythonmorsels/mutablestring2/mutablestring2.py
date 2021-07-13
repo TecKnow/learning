f4be04ca304bf890d65add05ee1618b83a3ff44b
David P. Perkins <tecknow@grumbleware.com>
2021-07-13 17:39:25 -0500

    Solution to the PythonMorsels problem "mutablestring2" solving base, first, and second bonus objectives.

--- pythonmorsels/mutablestring2/mutablestring2.py
+++ pythonmorsels/mutablestring2/mutablestring2.py
@@ -9,19 +9,34 @@ class MutableString(MutableSequence, UserString):
     def data_list(self):
         data_as_list = list(self.data)
         yield data_as_list
+        # data_as_list = [str(x) for x in data_as_list]
         self.data = ''.join(data_as_list)
+
+    def __getitem__(self, item):
+        return self.__class__(self.data[item])
 
     def __setitem__(self, key, value):
         with self.data_list() as data_as_list:
-            data_as_list[key] = value
+            data_as_list[key] = str(value)
+        return self
 
     def __delitem__(self, key):
         with self.data_list() as data_as_list:
             del data_as_list[key]
+        return self
 
     def __len__(self):
         return len(self.data)
 
     def insert(self, index, obj):
         with self.data_list() as data_as_list:
-            data_as_list.insert(index, obj)
+            data_as_list.insert(index, str(obj))
+        return self
+
+    def __imul__(self, other):
+        self.data = self.data * other
+        return self
+
+
+    def __str__(self):
+        return self.data
