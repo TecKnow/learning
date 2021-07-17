a283650bc04a948486ac574328be815e8d4d792b
David P. Perkins <tecknow@grumbleware.com>
2021-07-17 14:22:07 -0500

    Solution to the pythonmorsels problem "natural_sort" passing base and first bonus objective.

--- pythonmorsels/natural_sort/sortutils.py
+++ pythonmorsels/natural_sort/sortutils.py
@@ -1,4 +1,12 @@
-from typing import Iterable
+from typing import Iterable, Optional, Callable
 
-def natural_sort(iterable : Iterable[str], reverse : bool=False) -> Iterable[str]:
-    return sorted(iterable, reverse=reverse, key=str.casefold)
+
+def natural_sort(iterable: Iterable[str],
+                 reverse: bool = False,
+                 key: Optional[Callable[[str], str]] = None) -> Iterable[str]:
+    _key = natural_key if key is None else key
+    return sorted(iterable, reverse=reverse, key=_key)
+
+
+def natural_key(item: str) -> str:
+    return str.casefold(item)
