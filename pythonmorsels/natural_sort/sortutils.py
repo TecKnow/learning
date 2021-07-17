017a631ebeb6ea65c306da3bf3bd635b92026644
David P. Perkins <tecknow@grumbleware.com>
2021-07-17 17:52:21 -0500

    Solution to the pythonmorsels problem "natural_sort" passing all objectives.

--- pythonmorsels/natural_sort/sortutils.py
+++ pythonmorsels/natural_sort/sortutils.py
@@ -1,4 +1,19 @@
-from typing import Iterable, Optional, Callable
+import re
+from typing import Iterable, Optional, Callable, Union
+from functools import singledispatch
+
+
+# Human sorting method from https://nedbatchelder.com/blog/200712/human_sorting.html
+
+def _tryint(s: str) -> Union[int, str]:
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+@singledispatch
+def natural_key(s: str) -> list[Union[str, int]]:
+    return [_tryint(str.casefold(c)) for c in re.split('([0-9]+)', s)]
 
 
 def natural_sort(iterable: Iterable[str],
@@ -6,7 +21,3 @@ def natural_sort(iterable: Iterable[str],
                  key: Optional[Callable[[str], str]] = None) -> Iterable[str]:
     _key = natural_key if key is None else key
     return sorted(iterable, reverse=reverse, key=_key)
-
-
-def natural_key(item: str) -> str:
-    return str.casefold(item)
