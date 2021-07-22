70a86b2325faec356071295df1105c06722164a4
David P. Perkins <tecknow@grumbleware.com>
2021-07-22 14:25:19 -0500

    Solution to the pythonmorsels problem "validators)" passing all objectives.

--- pythonmorsels/validators/validators.py
+++ pythonmorsels/validators/validators.py
@@ -1,26 +1,34 @@
+from abc import ABC, abstractmethod
 from numbers import Number
-from typing import Any, Optional
 
 
-class PositiveNumber:
+class Validator(ABC):
+    _no_value = object()
 
-    _default_initial_value = object()
+    def __init__(self, initial_value=_no_value):
+        self.initial_value = initial_value
 
-    def __set_name__(self, owner: Any, name: str):
+    def __set_name__(self, owner, name):
         self.private_name = f"_{name}"
 
-    def __init__(self, initial_value: Optional[int] = None):
-        self.initial_value = initial_value if initial_value is not None else self._default_initial_value
-
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner=None):
         attr_value = getattr(instance, self.private_name, self.initial_value)
-        if attr_value is not self._default_initial_value:
+        if attr_value is not self._no_value:
             return attr_value
         else:
             raise AttributeError(f"'{owner}' object has no attribute '{self.private_name[1:]}'")
 
     def __set__(self, instance, value):
-        setattr(instance, self.private_name, self.validate(value))
+        validated_value = self.validate(value)
+        set_value = validated_value if validated_value is not None else value
+        setattr(instance, self.private_name, set_value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class PositiveNumber(Validator):
 
     @staticmethod
     def validate(value):
