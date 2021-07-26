9cc76b745b24946309b9c94ff8f03381bc41d4d2
David P. Perkins <tecknow@grumbleware.com>
2021-07-26 16:43:22 -0500

    Solution to the pythonmorsels problem "iconv.py" passing all objectives.
    Incorporating improvements from reference solutions.

--- pythonmorsels/iconv/iconv.py
+++ pythonmorsels/iconv/iconv.py
@@ -1,27 +1,23 @@
+import locale
 import argparse
 import sys
-from pprint import pprint
 
 parser = argparse.ArgumentParser(description="Convert between text encodings")
 
 parser.add_argument("-o", "--output", help="output file")
-parser.add_argument("-f", "--from-code", help="input encoding")
-parser.add_argument("-t", "--to-code", help="output encoding")
+parser.add_argument("-f", "--from-code", default=locale.getpreferredencoding(False), help="input encoding")
+parser.add_argument("-t", "--to-code", default=locale.getpreferredencoding(False), help="output encoding")
 parser.add_argument("-c", action="store_const", const="ignore", default="strict", help="Ignore errors")
-parser.add_argument("infile", nargs="?", help="File name of input data")
+parser.add_argument("infile", nargs="?", default="-", help="File name of input data")
 
 
 def process_args(args: argparse.Namespace) -> argparse.Namespace:
-    if args.from_code is None:
-        args.from_code = sys.getdefaultencoding()
-    if args.to_code is None:
-        args.to_code = sys.getdefaultencoding()
     if args.output is None:
         sys.stdout.reconfigure(encoding=args.to_code)
         args.output = sys.stdout
     else:
         args.output = open(args.output, "w", encoding=args.to_code, errors=args.c)
-    if args.infile is None or args.infile == "-":
+    if args.infile == "-":
         sys.stdin.reconfigure(encoding=args.from_code, errors=args.c)
         args.infile = sys.stdin
     else:
@@ -41,5 +37,3 @@ if __name__ == "__main__":
         args.output.close()
     if args.infile is not sys.stdin:
         args.infile.close()
-
-
