5fc2ca3c6bf7d0bab076ca2998bcb490931fc313
David P. Perkins <tecknow@grumbleware.com>
2021-07-30 04:40:21 -0500

    Solution to the pythonmorsels problem "ProxyDict" passing all objectives.

--- pythonmorsels/ProxyDict/proxydict.py
+++ pythonmorsels/ProxyDict/proxydict.py
@@ -1,13 +1,18 @@
-from collections import UserDict
 from collections import ChainMap
-from types import MappingProxyType
+from collections import UserDict
 from collections.abc import Mapping
+from types import MappingProxyType
 
 
 class ProxyDict(UserDict, Mapping):
-    def __init__(self, *args, **kwargs):
-        self.data_writeable = ChainMap(*args, **kwargs)
+    def __init__(self, *args, ):
+        super().__init__()
+        self.data_writeable = ChainMap(*reversed(args))
         self.data = MappingProxyType(self.data_writeable)
-        self.maps = self.data_writeable.maps
+        self.maps = list(args)
 
+    def __repr__(self):
+        return f"ProxyDict({repr(self.maps)[1:-1]})"
 
+    def __str__(self):
+        return repr(self)
