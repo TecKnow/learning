7aecf2488a776012abb486f4998eea82d75abfd1
David P. Perkins <tecknow@grumbleware.com>
2021-07-31 17:39:27 -0500

    Solution to the pythonmorsels problem "partial" passing base and first bonus objective.

--- pythonmorsels/partial/partial.py
+++ pythonmorsels/partial/partial.py
@@ -1,3 +1,4 @@
+import collections
 import typing
 
 SKIP = object()
@@ -9,6 +10,17 @@ class partial:
         self.func = func
         self.saved_args = args
         self.saved_kwargs = kwargs
+        self.skip_count = sum(1 for x in self.saved_args if x == SKIP)
 
     def __call__(self, *args, **kwargs) -> typing.Any:
-        return self.func(*self.saved_args, *args, **kwargs, **self.saved_kwargs)
+        if len(args) < self.skip_count:
+            raise TypeError("Not enough positional arguments given")
+        supplied_args = collections.deque(args)
+        combined_args = []
+        for arg in self.saved_args:
+            if arg == SKIP:
+                combined_args.append(supplied_args.popleft())
+            else:
+                combined_args.append(arg)
+        combined_args.extend(supplied_args)
+        return self.func(*combined_args, **kwargs, **self.saved_kwargs)
