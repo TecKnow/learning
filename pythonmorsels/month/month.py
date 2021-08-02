4ce7c4692f4f2008c9f6ae88ae38b6196d570043
David P. Perkins <tecknow@grumbleware.com>
2021-08-02 17:14:05 -0500

    Solution to the pythonmorsels problem "month" passing base, third bonus objectives.

--- pythonmorsels/month/month.py
+++ pythonmorsels/month/month.py
@@ -1,26 +1,14 @@
-from functools import total_ordering
+from dataclasses import dataclass
 
 
-@total_ordering
+@dataclass(order=True, frozen=True)
 class Month:
-    def __init__(self, year: int, month: int):
-        self.year = year
-        self.month = month
+    __slots__ = tuple("year month".split())
+    year: int
+    month: int
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.year}, {self.month})"
 
     def __str__(self):
         return f"{self.year:04d}-{self.month:02d}"
-
-    def __eq__(self, other):
-        if not isinstance(other, self.__class__):
-            return NotImplemented
-        else:
-            return (self.year, self.month) == (other.year, other.month)
-
-    def __lt__(self, other):
-        if not isinstance(other, self.__class__):
-            return NotImplemented
-        else:
-            return (self.year, self.month) < (other.year, other.month)
