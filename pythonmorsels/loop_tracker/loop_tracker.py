d306d6c094f26b01ae83872fe914d94d43c54e98
David P. Perkins <tecknow@grumbleware.com>
2021-08-04 20:45:07 -0500

    Solution to the pythonmorsels problem "loop_tracker" passing all objectives.
    Incorportating imporvements from the reference solution

--- pythonmorsels/loop_tracker/loop_tracker.py
+++ pythonmorsels/loop_tracker/loop_tracker.py
@@ -1,26 +1,34 @@
+import collections.abc
 from typing import Iterable, Iterator, Any
 
 
-class loop_tracker:
+class loop_tracker(collections.abc.Iterable):
     EMPTY = object()
 
     def __init__(self, underlying_iterable: Iterable[Any]):
-        self.length = 0  # type: int
+        self._length = 0  # type: int
         self.empty_accesses = 0  # type: int
-        self.underlying = iter(underlying_iterable)
+        self._underlying = iter(underlying_iterable)
         self._first = self.EMPTY
         self._last = self.EMPTY
-        self._next = self.EMPTY
-        try:
-            self._next = next(self.underlying)
-            self._first = self._next
-        except StopIteration:
-            pass
+        self._cached = self.EMPTY
+
+    def _get_next(self):
+        if self._first is self.EMPTY:
+            item = next(self._underlying, self.EMPTY)
+            self._first = item
+        elif self._cached is not self.EMPTY:
+            item = self._cached
+            self._cached = self.EMPTY
+        else:
+            item = next(self._underlying, self.EMPTY)
+        return item
 
     @property
     def first(self):
         if self._first is self.EMPTY:
-            raise AttributeError("iterable has no first attribute (it's empty)")
+            if self.is_empty():
+                raise AttributeError("iterable has no first attribute (it's empty)")
         return self._first
 
     @property
@@ -30,24 +38,22 @@ class loop_tracker:
         return self._last
 
     def __len__(self) -> int:
-        return self.length
+        return self._length
 
     def __iter__(self) -> Iterator:
         return self
 
     def __next__(self) -> Any:
-        if self._next is self.EMPTY:
+        item = self._get_next()
+        if item is self.EMPTY:
             self.empty_accesses += 1
-            raise StopIteration
+            return next(self._underlying)
         else:
-            self._last = self._next
-        try:
-            self._next = next(self.underlying)
-        except StopIteration as e:
-            self._next = self.EMPTY
-        finally:
-            self.length += 1
-            return self._last
+            self._last = item
+            self._length += 1
+            return item
 
     def is_empty(self):
-        return bool(self._next is self.EMPTY)
+        if self._cached is self.EMPTY:
+            self._cached = self._get_next()
+            return bool(self._cached is self.EMPTY)
