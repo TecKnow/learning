9d86f6a3db376b5c92ee0994e967b7a2332f5d03
David P. Perkins <tecknow@grumbleware.com>
2021-08-10 17:35:36 -0500

    Solution to the pythonmorsels problem "SortedList" passing all bonus objectives.
    Incoporating improvements from the reference solution.

--- pythonmorsels/bettercsv2/bettercsv2.py
+++ pythonmorsels/bettercsv2/bettercsv2.py
@@ -1,12 +1,22 @@
+import functools
 from collections.abc import Iterator
 from csv import reader
+from dataclasses import astuple, make_dataclass
 from typing import Optional, Iterable
+
+
+def row_factory(field_names: Iterable[str]):
+    def __iter__(self):
+        yield from astuple(self)
+
+    return make_dataclass("Row", field_names, namespace={"__slots__": field_names,
+                                                         "__iter__": __iter__})
 
 
 class FancyReader(Iterator):
     def __init__(self, rows: Iterable[str], fieldnames: Optional[Iterable[str]] = None, delimiter: Optional[str] = ','):
         self.row_reader = reader(rows, delimiter=delimiter)
-        self._fieldnames = fieldnames
+        self.fieldnames = fieldnames
         self._row_type = None
 
     def __next__(self):
@@ -22,25 +32,10 @@ class FancyReader(Iterator):
             self._fieldnames = next(self.row_reader)
         return self._fieldnames
 
-    @property
+    @fieldnames.setter
+    def fieldnames(self, fieldnames: Iterable[str]) -> None:
+        self._fieldnames = list(fieldnames) if fieldnames is not None else None
+
+    @functools.cached_property
     def row_type(self):
-        if self._row_type is None:
-            def row_type_init(instance, *args):
-                arg_list = list(args)
-                for attr in instance.__slots__:
-                    setattr(instance, attr, arg_list.pop(0))
-
-            def row_type_iter(instance):
-                yield from (getattr(instance, attr_name) for attr_name in instance.__slots__)
-
-            def row_type_repr(instance):
-                class_name = instance.__class__.__name__
-                attr_str_seq = [f"{a}='{getattr(instance, a)}'" for a in instance.__slots__]
-                attr_str = ", ".join(attr_str_seq)
-                return f"{class_name}({attr_str})"
-
-            self._row_type = type("Row", (), {"__slots__": self.fieldnames,
-                                              "__init__": row_type_init,
-                                              "__iter__": row_type_iter,
-                                              "__repr__": row_type_repr})
-        return self._row_type
+        return row_factory(self.fieldnames)
