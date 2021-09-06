c2e616f2cc258c3b24764334a1eb9d903aa3b47a
David P. Perkins <tecknow@grumbleware.com>
2021-09-06 14:14:39 -0500

    Solution to the pythonmorsels exercise "easyjson" passing all objectives.

--- pythonmorsels/easyjson/easyjson.py
+++ pythonmorsels/easyjson/easyjson.py
@@ -1,12 +1,48 @@
 import json
-from collections import UserDict
-from collections.abc import Mapping
+from collections import UserDict, UserList
+from collections.abc import MutableSequence, Mapping
 
 
 class SimpleDict(UserDict, Mapping):
     def __getattr__(self, item):
         return self[item]
 
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            return SimpleDict({x: self.data[x] for x in item})
+        else:
+            return self.data[item]
+
+
+class EllipsisList(UserList, MutableSequence):
+    def __getitem__(self, item):
+        if item is Ellipsis:
+            return EllipsisGetter(self)
+        else:
+            return self.data[item]
+
+
+class EllipsisGetter:
+    def __init__(self, backing_list):
+        self.backing_list = backing_list
+
+    def __getitem__(self, item):
+        return EllipsisList([element[item] for element in self.backing_list])
+
+    def __getattr__(self, item):
+        return self[item]
+
+
+def object_parser(pairs):
+    ret = SimpleDict(pairs)
+    for k, v in ret.items():
+        if isinstance(v, list):
+            ret[k] = EllipsisList(v)
+    return ret
+
 
 def parse(json_str: str) -> SimpleDict:
-    return json.loads(json_str, object_hook=lambda d: SimpleDict(**d))
+    ret = json.loads(json_str, object_pairs_hook=object_parser)
+    if isinstance(ret, list):
+        ret = EllipsisList(ret)
+    return ret
