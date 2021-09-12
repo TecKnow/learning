7680c8b7f6c901e826ba2e6efb97f60ccf791491
David P. Perkins <tecknow@grumbleware.com>
2021-09-12 07:41:03 -0500

    Solution to the Python Morsels problem "find_duplicates" passing all objectives.

--- pythonmorsels/find_duplicates/fileutils2.py
+++ pythonmorsels/find_duplicates/fileutils2.py
@@ -10,23 +10,29 @@ def get_file_hash(pathname):
 
 
 def find_duplicates(file_paths):
-    print(file_paths)
 
     # first, place every file into buckets by size.
 
-    # file_sizes = dict()
-    # for file_path in file_paths:
-    #     file_size = Path(file_path).stat().st_size
-    #     file_sizes.setdefault(file_size, set()).add(file_path)
-    #
+    file_sizes = dict()
+    for file_path in file_paths:
+        file_size = Path(file_path).stat().st_size
+        file_sizes.setdefault(file_size, set()).add(file_path)
+    from pprint import pprint
+    # print("Files by size")
+    # pprint(file_sizes)
+
     # # Files with unique sizes must also be unique, so stop considering them.
     # # Only groups of files with identical sizes may have identical contents.
-    # duplicate_file_sizes = {size: file_paths for size, file_paths in file_sizes if len(file_paths) > 1}
-    #
-    # file_sizes_and_hashes = dict()
-    # for size, file_paths in duplicate_file_sizes:
-    #     for file_path in file_paths:
-    #         file_sizes_and_hashes.setdefault((size, get_file_hash(file_path)), set()).add(file_path)
-    # return [path_set
-    #         for ((file_size, file_hash), path_set) in file_sizes_and_hashes
-    #         if len(path_set) > 1]
+    duplicate_file_sizes = {size: file_paths for size, file_paths in file_sizes.items() if len(file_paths) > 1}
+    print("files with duplicate sizes:")
+    # pprint(duplicate_file_sizes)
+
+    file_sizes_and_hashes = dict()
+    for size, file_paths in duplicate_file_sizes.items():
+        for file_path in file_paths:
+            file_sizes_and_hashes.setdefault((size, get_file_hash(file_path)), set()).add(file_path)
+        # print("Files and hashes")
+        # pprint(file_sizes_and_hashes)
+    return [path_set
+            for ((file_size, file_hash), path_set) in file_sizes_and_hashes.items()
+            if len(path_set) > 1]
