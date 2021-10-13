d2b2e42223548c1cb4d8edc192db8c873250ddcd
David P. Perkins <tecknow@grumbleware.com>
2021-10-13 15:34:55 -0500

    Solution to the Python Morsels problem "SequenceZip (part 2)" passing base, first,and second bonus objectives.
    Third objective in progress.

--- pythonmorsels/SequenceZip_Part2/sequencezip2.py
+++ pythonmorsels/SequenceZip_Part2/sequencezip2.py
@@ -46,3 +46,23 @@ class SequenceZip(Sequence):
         for elem in self._sequences:
             del elem[key]
 
+    def append(self, new_items):
+        if len(new_items) != len(self._sequences):
+            raise TypeError(f"Expected a sequence of {len(self._sequences)} items")
+        for contained_seq, extension in zip(self._sequences, new_items):
+            contained_seq.append(new_items)
+
+    def insert(self, position, new_item):
+        if len(new_item) != len(self._sequences):
+            raise TypeError(f"Expected a sequence of {len(self._sequences)} items")
+        for contained_seq, new_element in zip(self._sequences, new_item):
+            contained_seq.insert(position, new_element)
+
+
+if __name__ == "__main__":
+    x, y, z = [0, 1, 2, 3], [4, 5, 6], [7, 8, 9, 10]
+    seq = SequenceZip(x, y, z)
+    seq.insert(0, (9, 5, 0))
+    print(x)
+    print(y)
+    print(z)
