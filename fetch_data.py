74a3ab5cebae539449cd08d4cf94c815d1d2e5d3
David P. Perkins <tecknow@grumbleware.com>
2022-06-12 16:21:51 -0500

    Extract data fetching functions

---
+++ fetch_data.py
@@ -0,0 +1,16 @@
+import requests
+import re
+
+def get_page_source(url: str) -> str:
+    with requests.Session() as session:
+        return session.get(url).content.decode('utf-8')
+
+def get_data_blob(page_source: str) -> str:
+    comment_re = re.compile("<!--(.*?)-->", flags=re.DOTALL)
+    results = comment_re.findall(page_source)
+    return results[1]
+
+def get_data(url: str) -> str:
+    page_source = get_page_source(url)
+    data_blob = get_data_blob(page_source)
+    return data_blob
--- ocr.py
+++ ocr.py
@@ -1,17 +1,9 @@
 from collections import Counter
-import requests
-import re
+
+from fetch_data import get_data
 
 URL: str = "http://www.pythonchallenge.com/pc/def/ocr.html"
 
-def get_page_source(url: str = URL) -> str:
-    with requests.Session() as session:
-        return session.get(URL).content.decode('utf-8')
-
-def get_blob(page_source: str) -> list[str]:
-    comment_re = re.compile("<!--(.*?)-->", flags=re.DOTALL)
-    results = comment_re.findall(page_source)
-    return results[1]
 
 def count_characters(data: str) -> Counter:
     frequencies = Counter(data)
@@ -20,7 +12,6 @@ def count_characters(data: str) -> Counter:
 
 
 if __name__ == "__main__":
-    data = get_page_source(URL)
-    blob = get_blob(data)
-    frequencies = count_characters(blob)
+    data = get_data(URL)
+    frequencies = count_characters(data)
     print(frequencies.most_common())
