d487e7113c9d74f7b5e64f55e3ea2f11f571e79e
David P. Perkins <tecknow@grumbleware.com>
2022-06-12 17:23:29 -0500

    Add solution for level 4

--- equality.py
+++ equality.py
@@ -5,7 +5,7 @@ from fetch_data import get_data
 URL: str = "http://www.pythonchallenge.com/pc/def/equality.html"
 
 def bodyguards(data: str) -> str:
-    bodybuard_re = re.compile(r"(?<![A-Z])[A-Z]{3}([a-z])[A-Z]{3}(?![A-Z])", flags=re.DOTALL)
+    bodybuard_re = re.compile(r"(?<![A-Z])[A-Z]{3}([^A-Z])[A-Z]{3}(?![A-Z])", flags=re.DOTALL)
     return bodybuard_re.findall(data)
 
 
