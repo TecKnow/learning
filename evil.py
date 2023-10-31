be3f750655a302b763d69be8e40a0761ec12d981
David P. Perkins <tecknow@grumbleware.com>
2023-10-31 11:55:27 -0700

    Start solution for problem 12

---
+++ evil.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+URL: str = "http://www.pythonchallenge.com/pc/return/evil.html"
+IMAGE_URL: str = "http://www.pythonchallenge.com/pc/return/evil1.jpg"
