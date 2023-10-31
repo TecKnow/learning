c3726b5249e09f2d7198994f395fedde0f959d00
David P. Perkins <tecknow@grumbleware.com>
2023-10-31 11:50:43 -0700

    Add solution for level 11

--- 5808.py
+++ 5808.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 from PIL import Image
 from itertools import batched, chain
+from math import ceil, floor
 
 TMP = Path("/tmp")
 IMAGEFILE_PATH = TMP / "cave.jpg"
@@ -36,14 +37,15 @@ if __name__ == "__main__":
     _load_data()
     im = Image.open(IMAGEFILE_PATH)
     im_width, im_height = im.size
-    evens_pixels: list[list[tuple[int, int, int]]] = list()
-    odds_pixels: list[list[tuple[int, int, int]]] = list()
-    image_pixels = list(batched(list(im.getdata()), im_width))
+    image_pixels = list(im.getdata())
+    evens_pixels: list[tuple[int, int, int]] = list()
+    odds_pixels: list[tuple[int, int, int]] = list()
+    
     evens_pixels = image_pixels[::2]
     odds_pixels = image_pixels[1::2]
     odds_image = Image.new(im.mode, im.size)
-    odds_image.putdata(list(chain.from_iterable(odds_pixels)))
+    odds_image.putdata(odds_pixels)
     odds_image.save(ODDS_PATH)
     even_image = Image.new(im.mode, im.size)
-    even_image.putdata(list(chain.from_iterable(evens_pixels)))
+    even_image.putdata(evens_pixels)
     even_image.save(EVENS_PATH)
