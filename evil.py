2d0a32b0a5d9de95ee8b58b084929b75d2c920ba
David P. Perkins <tecknow@grumbleware.com>
2023-11-03 12:31:59 -0700

    Add solution for problem 12

--- evil.py
+++ evil.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -6,3 +8,24 @@ logger = logging.getLogger(__name__)
 
 URL: str = "http://www.pythonchallenge.com/pc/return/evil.html"
 IMAGE_URL: str = "http://www.pythonchallenge.com/pc/return/evil1.jpg"
+GFX_URL: str = "http://www.pythonchallenge.com/pc/return/evil2.gfx"
+
+TMP_PATH = Path("/tmp")
+GFX_PATH = TMP_PATH / "evil2.gfx"
+
+def _get_data():
+    if not GFX_PATH.exists():
+        logger.info("GFX file not found, attepmting to download")
+        with GFX_PATH.open("wb") as GFX_FILE:
+            GFX_FILE.write(requests.get(GFX_URL).content)
+    else:
+        logger.info("Found existing file")
+
+if __name__=="__main__":
+    _get_data()
+    with GFX_PATH.open("rb") as GFX_FILE:
+        GFX_DATA = GFX_FILE.read()
+        for i in range(5):
+            with (TMP_PATH / f"image{i}.jpg").open("wb") as IMG_FILE:
+                IMG_FILE.write(GFX_DATA[i::5])
+
